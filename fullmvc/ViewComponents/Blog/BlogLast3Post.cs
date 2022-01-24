4cfad936531574f9f716657fbfe6895dad17828c
Uğur Ören <77584569+uuroren@users.noreply.github.com>
2022-01-25 00:11:11 +0300

    Footer Last 3 Blog Post List
    Footer Last 3 Blog Post List

---
+++ fullmvc/ViewComponents/Blog/BlogLast3Post.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace fullmvc.ViewComponents.Blog
+{
+    public class BlogLast3Post : ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = bm.GetList3Blog();
+            return View(values);
+        }
+    }
+}
