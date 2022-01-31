49d1038381f4bd28f165903ab190f095162d9a63
Uğur Ören <77584569+uuroren@users.noreply.github.com>
2022-01-31 10:54:40 +0300

    blog listelemede revize

--- BusinessLayer/ValidationRules/BlogValidator.cs
+++ BusinessLayer/ValidationRules/BlogValidator.cs
@@ -17,8 +17,8 @@ namespace BusinessLayer.ValidationRules
             RuleFor(x => x.BlogImage).NotEmpty().WithMessage("Blog Resmini Boş Geçemezsiniz");
             RuleFor(x => x.BlogTitle).MaximumLength(150).WithMessage("Blog Başlığı 150 Karakterden Fazla Olamaz");
             RuleFor(x => x.BlogTitle).MinimumLength(5).WithMessage("Blog Başlığı 5 Karakterden Az Olamaz");
-            RuleFor(x => x.BlogContent).MaximumLength(250).WithMessage("Blog İçeriği 250 Karakterden Fazla Olamaz");
-            RuleFor(x => x.BlogContent).MinimumLength(25).WithMessage("Blog İçeriği 25 Karakterden Az Olamaz");
+            RuleFor(x => x.BlogContent).MaximumLength(500).WithMessage("Blog İçeriği 500 Karakterden Fazla Olamaz");
+            RuleFor(x => x.BlogContent).MinimumLength(100).WithMessage("Blog İçeriği 100 Karakterden Az Olamaz");
             RuleFor(x => x.CategoryId).NotEmpty().WithMessage("Bir Kategori Seçimi Yapın");
 
         }
